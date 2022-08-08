1b4f3ffa4a1881e5ee1757bc081e57b3fc24527c
Alan Flores <alan.2.flores@atos.net>
2022-08-08 11:28:34 -0500

    Add project files.

---
+++ Controllers/PerfilPersonalController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace WebPersonal.BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PerfilPersonalController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public string Get(int id) {
+            return id switch {
+                1 => "Alan",
+                2 => "Patrick",
+                _ => throw new NotSupportedException("El id no es valido")
+            };
+        }
+
+        [HttpPost]
+        public string Post(PerfilPersonalDto perfilPersonal) {
+            return perfilPersonal.Nombre;
+        }
+    }
+
+    public class PerfilPersonalDto {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+    }
+}
